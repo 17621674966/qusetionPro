0963ba6eb53671ebc7ec094b8639034dd0a35b37
xyz <1019690363@qq.com>
2020-11-11 20:36:07 +0800

    2020

---
+++ frame/Infrastructure/ExpandableHelper.cs
@@ -0,0 +1,17 @@
+﻿using LinqKit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure
+{
+   public  class ExpandableHelper
+    {
+        public static IQueryable<T> GetExpandable<T>(IQueryable<T> obj)
+        {
+            return obj.AsExpandable() as IQueryable<T>;
+        }
+    }
+}
---
+++ frame/Infrastructure/FileHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure
+{
+    public class FileHelper
+    {
+        public static string GetFileType(string file)
+        {
+            string fileType = string.Empty;
+            if (string.IsNullOrEmpty(file))
+            {
+                throw new Exception("文件名称不能为空！");
+            }
+            int index = file.LastIndexOf(".");
+            return file.Substring(index);
+
+
+        }
+    }
+}
