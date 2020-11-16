f12e9dac6c2203afaca705ca3ab1a39f2d8bd46b
xyz <1019690363@qq.com>
2020-11-17 00:26:38 +0800

    Signed-off-by: xyz <1019690363@qq.com>

--- frame/OpenAuth.Mvc/Controllers/AccountController.cs
+++ frame/OpenAuth.Mvc/Controllers/AccountController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Infrastructure;
+using OpenAuth.App;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,14 +8,56 @@ using System.Web.Mvc;
 
 namespace OpenAuth.Mvc.Controllers
 {
+    [AllowAnonymous]
     public class AccountController : Controller
     {
 
+        public LoginApp App { get; set; }
         // GET: Account
         public ActionResult Index()
         {
             return View();
         }
+
+
+        /// <summary>
+        /// 验证登录
+        /// </summary>
+        /// <param name="userName"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        public JsonResult CheckLogin(string userName,string password)
+        {
+            HttpContext.Response.AppendHeader("Access-Control-Allow-Origin", "*");
+            if (string.IsNullOrWhiteSpace(userName)||string.IsNullOrWhiteSpace(password))
+            {
+                Response Result = new Response();
+                Result.Code = 500;
+                Result.Message = "用户名或密码不能为空！";
+                return Json(Result, JsonRequestBehavior.DenyGet);
+            }
+
+            var user = App.Repository.
+                FindSingle(x => x.Account.Equals(userName) && x.Password.Equals(password));
+
+            if (user == null)
+            {
+                Response Result = new Response();
+                Result.Code = 500;
+                Result.Message = "用户名或密码不正确！";
+                return Json(Result, JsonRequestBehavior.DenyGet);
+            }
+            else {
+                Response Result = new Response();
+                Result.Code = 200;
+                Result.Message = "登录成功！";
+                Result.Obj = new { Id= user .Id};
+                return Json(Result, JsonRequestBehavior.DenyGet);
+            }
+
+        }
+
+
 
     }
 }
---
+++ frame/OpenAuth.Mvc/Controllers/QsMainLogController.cs
@@ -0,0 +1,103 @@
+﻿using Infrastructure;
+using Newtonsoft.Json;
+using OpenAuth.App;
+using OpenAuth.Repository.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Transactions;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OpenAuth.Mvc.Controllers
+{
+    public class QsMainLogController : Controller
+    {
+        // GET: QsMainLog
+
+        public T_QsMainLogApp App { get; set; }
+        public T_QsDetailLog1App AppDetail { get; set; }
+
+        public LoginApp AppLog { get; set; }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        /// <summary>
+        /// 添加问卷主表信息
+        /// </summary>
+        /// <param name="mainLog"></param>
+        /// <returns></returns>
+        public JsonResult AddMessage(string mainLog, string detailLog, string userId)
+        {
+
+            HttpContext.Response.AppendHeader("Access-Control-Allow-Origin", "*");
+            var main = JsonConvert.DeserializeObject<T_QsMainLog>(mainLog);
+            var detail = JsonConvert.DeserializeObject<T_QsDetailLog1>(detailLog);
+
+            var user = AppLog.Repository.FindSingle(x => x.Id.Equals(userId));
+            if (user == null)
+            {
+                Response Result = new Response();  //未授权
+                Result.Code = 401;
+                Result.Message = "请先登录！";
+                return Json(Result, JsonRequestBehavior.DenyGet);
+            }
+
+            using (var train = new TransactionScope())
+            {
+
+
+                try
+                {
+                    //主表
+                    main.Id = Guid.NewGuid().ToString();
+                    main.AddUserName = user.Name;
+                    main.AddUserGuid = user.Id;
+                    main.AddTime = DateTime.Now;
+                    main.UpdateTime = DateTime.Now;
+                    main.RowStatus = 1;
+
+                    App.Repository.Add(main);
+
+
+                    //从表
+                    detail.MainId = main.Id;
+                    detail.Id = Guid.NewGuid().ToString();
+                    detail.AddTime = DateTime.Now;
+                    detail.AddUserName = user.Name;
+                    detail.AddUserGuid = user.Id;
+                    detail.UpdateTime = DateTime.Now;
+                    detail.RowStatus = 1;
+
+                    AppDetail.Repository.Add(detail);
+
+
+                    train.Complete();
+
+                }
+                catch (Exception ex)
+                {
+                    LogHelper.Log(ex.Message);
+                    Response Result = new Response();  
+                    Result.Code = 500;
+                    Result.Message = "添加失败！";
+                    return Json(Result, JsonRequestBehavior.DenyGet);
+                }
+
+
+                Response ResultEnd = new Response();
+                ResultEnd.Code = 200;
+                ResultEnd.Message = "添加成功！";
+                return Json(ResultEnd, JsonRequestBehavior.DenyGet);
+
+
+            }
+
+
+        }
+    }
+}
